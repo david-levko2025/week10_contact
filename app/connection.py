8634f1ec72ee0caceef7a3de7b82ecf71efb92b0
My Name <mdavidl@gmail.com>
2025-12-24 17:52:58 +0200

    yo

--- app/connection.py
+++ app/connection.py
@@ -7,11 +7,10 @@ load_dotenv()
 
 def get_connection():
     connection = mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
+        host=os.getenv("DB_HOST","db-1"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD", '12345678'),
-        database=os.getenv("DB_NAME"),
-        port=os.getenv("DB_PORT",3306)
+        database=os.getenv("DB_NAME")
     )
     return connection       
 
